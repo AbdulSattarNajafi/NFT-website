330d3e46ea04cf042d1d3cd6e2a663767ad3a080
Sattar <abdulsattarnajafi@gmail.com>
2022-01-03 06:41:10 +0700

    issues fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -149,10 +149,10 @@ $(".team-item-container").owlCarousel({
       items: 1,
     },
     600: {
-      items: 3,
+      items: 2,
     },
     1000: {
-      items: 5,
+      items: 3,
     },
   },
 });
